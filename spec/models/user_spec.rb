6291d1d108eeffa4818205be53915f77fea53735
Greg <knudsegi@hotmail.com>
2014-07-18 11:07:26 -0400

    user create test passes - issues with require rails_helper - tests still run, but many error messages also display

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
+require 'rails_helper'
+
 
 RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) {User.create(first_name: "test", email: "test@test.com",
+                          username: "testy", password: "test")}
+
+  it "creates a user with a first name, email, username, and password" do
+    expect(user).to be_instance_of(User)
+  end
+  context "validations"
+    xit "creates a user with a valid email address"
+    xit "raises an error when first name is not included"
+    xit "raises an error when email is not included"
+    xit "raises an error when password is not included"
+    xit "does not allow profanity to be used in first name field"
+
 end

c6673e189e32ba6412302636c3a647c2d3dabc04
Brendan Susens-Jackson <bsusensjackson@gmail.com>
2014-07-18 16:32:59 -0400

    created working test for comment content

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,10 +2,15 @@ require 'spec_helper'
 require 'rails_helper'
 
 describe Comment do
-  # let(:comment) {FactoryGirl.create :comment}
+  let(:user) {FactoryGirl.create :user}
+  let(:comment) {FactoryGirl.create :comment}
 
   it "creates a comment with text" do
-    @comment = FactoryGirl.create(:comment)
-    expect(@comment.content).to eq "lol parsing csv is the worst. u must be super smart and kool though, let's hang out and drink fernet branca soon."
+
+    expect(comment.content).to eq "lol parsing csv is the worst. u must be super smart and kool though, let's hang out and drink fernet branca soon."
+  end
+
+  it "correctly links a comment with a user." do
+    expect(comment.user.id).to eq user.id
   end
 end

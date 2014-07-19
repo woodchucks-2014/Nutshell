09e00986f0cd017d336f7bfba6141792288e5750
Richard <RJNY86@gmail.com>
2014-07-19 12:00:01 -0400

    'Nut' root displays images of all users

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ActiveRecord::Base
 
   has_secure_password
   validates :email, format: {with: /[-0-9a-zA-Z.+_]+@[-0-9a-zA-Z.+_]+\.[a-zA-Z]{2,}/}
-  validates_presence_of :email, :first_name, :password, :password_confirmation
+  validates_presence_of :email, :first_name, :password, :password_confirmation, :username
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,19 +7,32 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 10.times do
-  User.create(
-              first_name: Faker::Internet.first_name,
-              username: Faker::Internet.username,
-              email: Faker::Internet.email
-              password: "password"
+  p User.create(
+              first_name: Faker::Name.first_name,
+              username: Faker::Internet.user_name,
+              email: Faker::Internet.email,
+              password: "password",
+              password_confirmation: "password"
               )
 end
 
-random_user = rand(User.count)
+def random_user
+  user_count = User.all.length
+  array = [*1..user_count]
+  array.sample
+end
+
+def generate_image_array
+  array = []
+  5.times do
+    array <<Faker::Avatar.image
+  end
+  array
+end
 
 10.times do
   Nut.create(content: Faker::Lorem.sentence,
-            user_id:
-            url: [] << Faker::Avatar.image
+            user_id: random_user,
+            url: generate_image_array
             )
 end

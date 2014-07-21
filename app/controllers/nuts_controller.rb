da0fa0d2f6a9fd9f012930434839dd3e9b861beb
Kai Prout <kaiprt@gmail.com>
2014-07-20 23:16:07 -0400

    Working image upload... actually this time. Other commit was a lie.

--- app/controllers/nuts_controller.rb
+++ app/controllers/nuts_controller.rb
@@ -18,39 +18,23 @@ class NutsController < ApplicationController
   def show
   end
 
-  def local_image_path(name)
-      Rails.root.join('uploads', name).to_s
-  end
-
   def create
-    if params[:file].present?
-      # preloaded = Cloudinary::PreloadedFile.new(params[:image_id])
-      # raise "Invalid upload signature" if !preloaded.valid?
-      # @model.image_id = preloaded.identifier
-      # @image = Cloudinary::Uploader.upload(params[:file])
-      # @picture = @image["secure_url"]
-      # @uploader = PictureUploader.new
-      # @uploader.store!(params[:file])
-      # @uploader.retrieve_from_store!(params[:file])
-      # redirect_to user_path(current_user)
+    uploaded_picture = params[:picture]
+    p "------------------------------"
+    p filename = Rails.root.join('public', 'uploads', uploaded_picture.original_filename)
+    p "-------------------------------"
+    File.open(filename, 'wb') do |file|
+      file.write(uploaded_picture.read)
     end
-    config_path = File.expand_path(File.join(File.dirname(__FILE__), params[:file]))
-    @nut = Nut.new
-    @nut.picture = params[:file]
-    p Rails.root
-    path = (Rails.root + params[:file]).to_s
-    path = local_image_path(params[:file])
-    p path
-    @image = Cloudinary::Uploader.upload(config_path)
-    # @nut.picture = File.open('')
-    # @nut.save!
-    p @image["secure_url"]
-    # p @nut #not finished need to add content
-    # @nut.url << @picture
-    # if @uploader.save!
-    #   redirect_to user_path(current_user)
-    # end
+# Insert code here to send to
+    p "------------------------------"
+  p image = Cloudinary::Uploader.upload(filename)
+    p "------------------------------"
+  @nut = Nut.new(url: [image["secure_url"]], user_id: current_user.id)
+    if @nut.save
+      redirect_to nuts_path
+    else
+      redirect_to user_path
+    end
   end
-
-
 end
